57bf29fba9edd792422d9aedb471cabe7a8d2f66
Jason Javandel <jasonjavandel@protonmail.com>
2025-09-23 13:01:59 -0700

    test change to server

--- application/web/src/routes/+page.svelte
+++ application/web/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="min-w-screen min-h-screen bg-white">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
