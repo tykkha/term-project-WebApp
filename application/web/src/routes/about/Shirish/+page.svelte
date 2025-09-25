896504bf76ed5932acbfb7a57a82e39967fe9d1e
Shirish <shirishmaharjan5@gmail.com>
2025-09-25 10:03:30 -0700

    Added About me

---
+++ application/web/src/routes/about/Shirish/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Person from '../../../components/person.svelte';
+</script>
+
+<div class="min-w-screen min-h-screen bg-white">
+	<h1 class="w-full pt-8 text-center text-5xl font-bold">About Me</h1>
+	<div class="mx-auto flex w-full max-w-3xl justify-center text-center pt-6">
+		<Person
+			name="Shirish Maharjan"
+			github="https://github.com/shirishsm5"
+			src="https://avatars.githubusercontent.com/shirishsm5"
+		/>
+	</div>
+</div>
