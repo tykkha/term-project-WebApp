837d223fbd912cb0d13cd54b74b1dd3a6858316d
tykkha <connorpm@duck.com>
2025-11-04 14:55:05 -0800

    [Update] Addition of v1 Schema

---
+++ application/api/app/schema/schema.sql
@@ -0,0 +1,76 @@
+DROP DATABASE IF EXISTS GatorGuides;
+CREATE DATABASE GatorGuides;
+USE GatorGuides;
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User
+(
+    uid      INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email    VARCHAR(255) UNIQUE            NOT NULL,
+    password VARCHAR(255)                   NOT NULL,
+    Type     ENUM ('user', 'admin')
+);
+
+DROP TABLE IF EXISTS Tags;
+CREATE TABLE Tags
+(
+    tagsID INT PRIMARY KEY AUTO_INCREMENT,
+    tags   VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS Tutor;
+CREATE TABLE Tutor
+(
+    tid    INT PRIMARY KEY AUTO_INCREMENT,
+    uid    INT NOT NULL,
+    rating DOUBLE,
+    FOREIGN KEY (uid) REFERENCES User (uid) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Posts;
+CREATE TABLE Posts
+(
+    pid       INT PRIMARY KEY AUTO_INCREMENT,
+    tid       INT NOT NULL,
+    tags      INT NOT NULL,
+    bio       TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (tid) REFERENCES Tutor (tid) ON DELETE CASCADE,
+    FOREIGN KEY (tags) REFERENCES Tags (tagsID) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Profile;
+CREATE TABLE Profile
+(
+    profileID INT PRIMARY KEY AUTO_INCREMENT,
+    uid       INT NOT NULL,
+    tags      INT NOT NULL,
+    status    VARCHAR(50),
+    bio       TEXT,
+    FOREIGN KEY (uid) REFERENCES User (uid) ON DELETE CASCADE,
+    FOREIGN KEY (tags) REFERENCES Tags (tagsID) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Sessions;
+CREATE TABLE Sessions
+(
+    sid       INT PRIMARY KEY AUTO_INCREMENT,
+    tid       INT,
+    uid       INT,
+    tags      INT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    concluded DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (tid) REFERENCES Tutor (tid) ON DELETE CASCADE,
+    FOREIGN KEY (uid) REFERENCES User (uid) ON DELETE CASCADE,
+    FOREIGN KEY (tags) REFERENCES Tags (tagsID) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Messages;
+CREATE TABLE Messages
+(
+    mid       INT PRIMARY KEY AUTO_INCREMENT,
+    uid       INT,
+    content   TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (uid) REFERENCES User (uid) ON DELETE CASCADE
+);
