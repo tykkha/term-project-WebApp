45977ce537c6bb5247c60822a76b5ff6319bec0c
Jason Javandel <jasonjavandel@protonmail.com>
2025-11-12 10:21:39 -0800

    [Update] Add dashboard example page to svelte

---
+++ application/web/src/routes/dashboard/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<div class="min-h-screen bg-neutral-100 p-6">
+	<div class="mx-auto max-w-7xl">
+		<section class="mb-8 rounded-2xl bg-white p-6 shadow-lg">
+			<h2 class="mb-4 text-2xl font-bold text-gray-800">Filter Results</h2>
+		</section>
+
+		<section class="mt-8 rounded-2xl bg-white p-6 shadow-lg">
+			<h2 class="mb-4 text-2xl font-bold text-gray-800">Filter Results</h2>
+		</section>
+	</div>
+</div>
