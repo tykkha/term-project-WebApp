d9dbaf182032cda0195e8f5233575edf19650a5d
Andy Yip <andy@Andys-MacBook-Pro.local>
2025-11-14 21:47:39 -0800

    [Update] Added nonfunctional tutor, review, and session pages

---
+++ application/web/src/routes/tutor/review/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<!--Review page-->
+<div class="min-h-screen min-w-screen bg-neutral-100">
+    <div class="p-4">
+        <a href="/tutor" class="hover:underline">Back</a>
+    </div>
+	<div class="flex gap-8 p-8 justify-center">
+        <div class="w-full rounded-2xl bg-white p-4 drop-shadow-lg md:w-6/12">
+            <h2 class="text-3xl underline">Write your Review for, "Name"</h2>
+            <form action="">
+                <div class="flex-col">
+                    <!--Should be checked to be between 1.0-5.0-->
+                    <label for="rating" class="block text-xl">Rating</label>
+                    <input type="text" class="">
+                    <!--Character limit ~200?-->
+                    <label for="description" class="block text-xl">Your Experience</label>
+                    <textarea id="" rows="5" placeholder="Share your experience" class="border-indigo-600 hover:border-indigo-700"></textarea>         
+                    <input type="submit" class="w-full">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ application/web/src/routes/tutor/session/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<!--Session page-->
+<div class="min-h-screen min-w-screen bg-neutral-100">
+    <div class="p-4">
+        <a href="/tutor" class="hover:underline">Back</a>
+    </div>
+	<div class="flex gap-8 p-8 justify-center">
+        <div class="w-full rounded-2xl bg-white p-4 drop-shadow-lg md:w-6/12">
+            <h2 class="text-3xl underline">Add a tutor session</h2>
+            <form action="">
+                <div class="flex-col">
+                    <!--Should check input for possible date-->
+                    <label for="date" class="block text-xl">Date</label>
+                    <input type="text" class="bg">
+                    <!---->                        
+                    <label for="time" class="block text-xl">Time</label>
+                    <input type="text" id="">
+                    <!--Optional-->
+                    <label for="location" class="block text-xl">Location (Optional)</label>
+                    <input type="text">
+                    <input type="submit" class="w-full">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
