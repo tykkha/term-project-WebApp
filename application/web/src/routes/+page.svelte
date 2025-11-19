c613743b7c44b75ae1d78237f8da4aec6b16fcc7
Jason Javandel <jasonjavandel@protonmail.com>
2025-11-18 17:55:15 -0800

    [Fix] Add tutoring to landing page

--- application/web/src/routes/+page.svelte
+++ application/web/src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <div class="min-w-screen min-h-screen bg-neutral-100 pt-16">
 	<div class="mx-auto flex w-full max-w-7xl flex-row gap-16">
 		<div class="w-1/2 pt-32">
-			<h1 class="text-5xl text-gray-600">Connecting students with each other</h1>
+			<h1 class="text-5xl text-gray-600">Connecting students with tutors</h1>
 
 			<p class="pt-16 text-2xl">
-				Here at GatorGuides, our focus is to connect students with each other to help boost success
-				.
+				Here at GatorGuides, our focus is to connect students and tutors with each other to help
+				boost success.
 			</p>
 		</div>
 		<div class="w-1/2">
