beee694858b7d956410eab1e03dfd667c6848fa9
Jason Javandel <jasonjavandel@protonmail.com>
2025-11-22 14:19:51 -0800

    [Fix] Make all about pages follow same template

--- application/web/src/routes/about/manea/+page.svelte
+++ application/web/src/routes/about/manea/+page.svelte
@@ -2,13 +2,16 @@
 	import Person from '../../../lib/components/Person.svelte';
 </script>
 
-<div class="min-w-screen min-h-screen bg-white">
-	<h1 class="w-full pt-8 text-center text-5xl font-bold">About Us</h1>
-	<div class="mx-auto flex w-full max-w-3xl justify-center text-center">
-		<Person
-			name="Manea"
-			github="https://github.com/manea127"
-			src="https://avatars.githubusercontent.com/u/127?v=4"
-		/>
+<div class="min-w-screen min-h-screen bg-neutral-100">
+	<div class=" pt-32">
+		<div
+			class="mx-auto mb-8 flex w-full max-w-3xl justify-center rounded-2xl bg-white p-6 text-center shadow-lg"
+		>
+			<Person
+				name="Manea"
+				github="https://github.com/manea127"
+				src="https://avatars.githubusercontent.com/u/127?v=4"
+			/>
+		</div>
 	</div>
 </div>
--- application/web/src/routes/about/shirish/+page.svelte
+++ application/web/src/routes/about/shirish/+page.svelte
@@ -2,13 +2,16 @@
 	import Person from '../../../lib/components/Person.svelte';
 </script>
 
-<div class="min-w-screen min-h-screen bg-white">
-	<h1 class="w-full pt-8 text-center text-5xl font-bold">About me</h1>
-	<div class="mx-auto flex w-full max-w-3xl justify-center pt-6 text-center">
-		<Person
-			name="Shirish Maharjan"
-			github="https://github.com/shirishsm5"
-			src="https://avatars.githubusercontent.com/shirishsm5"
-		/>
+<div class="min-w-screen min-h-screen bg-neutral-100">
+	<div class=" pt-32">
+		<div
+			class="mx-auto mb-8 flex w-full max-w-3xl justify-center rounded-2xl bg-white p-6 text-center shadow-lg"
+		>
+			<Person
+				name="Shirish Maharjan"
+				github="https://github.com/shirishsm5"
+				src="https://avatars.githubusercontent.com/shirishsm5"
+			/>
+		</div>
 	</div>
 </div>
