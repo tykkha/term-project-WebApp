fff20fd4feb9b804fbde9a1bebf64787c2bccb99
Tyca <connorpm@duck.com>
2025-11-24 00:31:11 -0800

    [Updated] Connor readme

--- application/web/src/routes/about/connor/+page.svelte
+++ application/web/src/routes/about/connor/+page.svelte
@@ -11,7 +11,11 @@
 				name="Connor Moore"
 				github="https://github.com/tykkha"
 				src="https://avatars.githubusercontent.com/u/100100421?v=4"
-			/>
+				description={[
+					"Howdy! My name is Connor M and I'm a senior at SF State University majoring in computer science with a focus on backend and database development.",
+					'Believe it or not, I focused more on electrical engineering in my earlier years of junior high and high school through their robotics programs, but it also introduced me to programming as a means of connecting software and the hardware we worked to construct for competitions and sparked the passion for doing what I do today.', 
+					"As much as I love software development and databases, I also like to keep a good life balance by enjoying my other hobbies such as gardening, coffee brewing, and going on adventures with my friends even if it's just to downtown San Francisco."
+				]}
 			/>
 		</div>
 	</div>
