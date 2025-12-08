c30391d10c19734f71470567941c9d8480e5857d
Andy Yip <andy@Andys-MacBook-Pro.local>
2025-12-07 20:08:19 -0800

    about bio

--- application/web/src/routes/about/andy/+page.svelte
+++ application/web/src/routes/about/andy/+page.svelte
@@ -12,7 +12,8 @@
 				github="https://github.com/andyyip915"
 				src="https://avatars.githubusercontent.com/u/120161192?v=4"
 				description={[
-					"Hello, my name is Andy Yip. I am a fourth year student computer science major at San Francisco State University. "
+					"Hello, my name is Andy Yip. I am a fourth year student computer science major at San Francisco State University. The fields that I am interested in are datatbases engineering and cybersecurity.",
+					"Some side interests that I have are videogame design and 3D modeling. Originally, these side interests had got me invested in acquring a computer science degree, though other fields of study have appeared to be more lucrative and stable. If I am given the chance, I would like to pursue these initial interests."
 				]}
 			/>
 		</div>
