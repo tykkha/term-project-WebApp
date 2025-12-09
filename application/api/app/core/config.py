0a24322ff2328e03d9afda387e47cfe51a7fb049
Jason Javandel <jasonjavandel@protonmail.com>
2025-12-09 11:14:15 -0800

    test api changes

--- application/api/app/core/config.py
+++ application/api/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_HOST: str = "localhost"
     DATABASE_USER: str = "root"
-    DATABASE_PASSWORD: str = ""
+    DATABASE_PASSWORD: str = "Tinykitten73&"
     DATABASE_NAME: str = "gatorguides"
     
     class Config:
