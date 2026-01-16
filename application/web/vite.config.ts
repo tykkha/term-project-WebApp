1b9724f8bf8b58487216babf565409fdebc584dd
Tykha <cmoore14@sfsu.edu>
2026-01-16 14:11:53 -0800

    Demo branch for pages

--- application/web/vite.config.ts
+++ application/web/vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: '/csc648-fa25-145-team09/',
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		proxy: {
