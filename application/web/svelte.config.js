a7c17b1addc4b8b35b3a57d0de8244c627adc41b
Tykha <cmoore14@sfsu.edu>
2026-01-16 14:44:08 -0800

    Add explicit base path configuration to svelte.config.js

--- application/web/svelte.config.js
+++ application/web/svelte.config.js
@@ -12,8 +12,12 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'
-		})
+			fallback: 'index.html',
+			strict: false
+		}),
+		paths: {
+			base: '/csc648-fa25-145-team09'
+		}
 	}
 };
 
