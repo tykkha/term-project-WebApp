32857dd7eb4087758cc52fe972f89c66b23820c3
Tykha <cmoore14@sfsu.edu>
2026-01-16 14:48:14 -0800

    Remove vite base config, use only svelte.config.js paths.base

--- application/web/vite.config.ts
+++ application/web/vite.config.ts
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/csc648-fa25-145-team09/',
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		proxy: {
